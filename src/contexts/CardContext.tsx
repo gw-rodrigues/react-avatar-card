b15c1f095e80c0fb56d65dbe2ac0fa12e2d77c03
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-24 19:17:32 +0100

    feat: create context fetch movie and casts

--- src/contexts/CardContext.tsx
+++ src/contexts/CardContext.tsx
@@ -1,5 +1,6 @@
-import { ReactNode, useState } from 'react'
+import { ReactNode, useCallback, useEffect, useState } from 'react'
 import { createContext } from 'use-context-selector'
+import { api } from '../lib/axios'
 
 interface IGenre {
   id: number
@@ -34,7 +35,7 @@ interface IMovie {
 }
 
 interface ICardContextTypes {
-  movies: IMovie[]
+  movie: IMovie
 }
 
 interface ICardContextProviderProps {
@@ -43,9 +44,37 @@ interface ICardContextProviderProps {
 
 export const CardContext = createContext({} as ICardContextTypes)
 
+const MOVIE_ID = 76600
+
 export function CardContextProvider({ children }: ICardContextProviderProps) {
-  const [movies, setMovies] = useState<IMovie[]>({} as IMovie[])
-  return (
-    <CardContext.Provider value={{ movies }}>{children}</CardContext.Provider>
+  const [movies, setMovies] = useState<ICardContextTypes>(
+    {} as ICardContextTypes,
   )
+
+  const fetchMovieByID = useCallback(async (id: number) => {
+    const movie = await api.get(`movie/${MOVIE_ID}`, {
+      params: { language: 'en-US' },
+    })
+    setMovies((prev) => ({ ...prev, ...movie.data }))
+  }, [])
+
+  const fetchCreditsByMovieId = useCallback(async (id: number) => {
+    const credits = await api.get(`movie/${MOVIE_ID}/credits`, {
+      params: { language: 'en-US' },
+    })
+    setMovies((prev) => ({ ...prev, casts: credits.data?.cast }))
+  }, [])
+
+  async function loadInitialData() {
+    await Promise.all([
+      fetchMovieByID(MOVIE_ID),
+      fetchCreditsByMovieId(MOVIE_ID),
+    ])
+  }
+
+  useEffect(() => {
+    loadInitialData()
+  }, [])
+
+  return <CardContext.Provider value={movies}>{children}</CardContext.Provider>
 }
