8641ed3fdbb04a48339e9fd2bb194a1dd8aed771
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-24 22:35:48 +0100

    build: add tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{tsx}'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: 'Nunito, sans-serif',
+      },
+    },
   },
   plugins: [],
 }
