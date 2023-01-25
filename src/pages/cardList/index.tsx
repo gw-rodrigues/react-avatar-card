951ac104fc56744b52a11385df247a3b7d669865
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-25 22:31:23 +0100

    feat: fix background style

--- src/pages/cardList/index.tsx
+++ src/pages/cardList/index.tsx
@@ -15,7 +15,7 @@ export function CardList() {
     <main className="flex flex-col items-center gap-20 py-20 min-w-full min-h-screen text-base bg-gradient-to-b from-sky-500 to-slate-900 relative overflow-hidden">
       <div className="absolute top-0 z-0 w-full min-h-full text-center after:z-10 after:w-full after:min-h-full after:top-0 after:left-0 after:absolute after:bg-gradient-to-b after:from-slate-900/70 after:via-slate-900/100 after:to-slate-900">
         <img
-          className="max-w-none w-full min-h-full relative"
+          className="max-w-none landscape:w-full portrait:w-auto  min-h-full relative"
           src={backgroundImg}
           alt="Movie Poster"
         />
