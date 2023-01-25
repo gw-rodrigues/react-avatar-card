fa704c3c52676e071fe11805d23f2a95a1e80f83
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-25 21:28:19 +0100

    feat: add background

--- src/pages/cardList/index.tsx
+++ src/pages/cardList/index.tsx
@@ -5,8 +5,21 @@ import { CardContext } from '../../contexts/CardContext'
 import { Card } from './card'
 
 export function CardList() {
+  const backgroundImg = useContextSelector(
+    CardContext,
+    (context) =>
+      `${context.movie.urls.bannerURL}/${context.movie.backdrop_path}`,
+  )
+
   return (
-    <main className="flex flex-col items-center min-w-full min-h-screen text-base bg-gradient-to-b from-sky-500 to-slate-900">
+    <main className="flex flex-col items-center gap-28 py-20 min-w-full min-h-screen text-base bg-gradient-to-b from-sky-500 to-slate-900 relative overflow-hidden">
+      <div className="absolute top-0 z-0 w-auto min-h-screen text-center after:z-10 after:w-full after:min-h-screen after:top-0 after:left-0 after:absolute after:bg-gradient-to-b after:from-slate-900/0 after:via-slate-900/100 after:to-slate-900">
+        <img
+          className="max-w-none min-h-full relative"
+          src={backgroundImg}
+          alt="Movie Poster"
+        />
+      </div>
       <Header />
       <Card />
       <Footer />
