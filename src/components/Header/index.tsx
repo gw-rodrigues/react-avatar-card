2c2f8dd64b38c0a3bc7473433e4522ae4ef29404
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-25 21:28:10 +0100

    feat: add header content

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,23 @@
 export function Header() {
-  return <header>ola</header>
+  return (
+    <header className="flex flex-col gap-4 items-center relative">
+      <div className="flex flex-col items-center">
+        <h1 className="text-4xl uppercase text-center font-bold drop-shadow-lg text-sky-300/90 border-b border-sky-500 ">
+          Movie Card
+        </h1>
+        <span className="text-xl uppercase font-bold text-sky-400/90 drop-shadow-lg">
+          Preview
+        </span>
+      </div>
+
+      <a className="flex" href="https://www.themoviedb.org/" target="_blank">
+        <span className="font-bold text-md text-green-300/80">API - </span>
+        <img
+          className="w-24"
+          src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg"
+          alt="TMDB"
+        />
+      </a>
+    </header>
+  )
 }
