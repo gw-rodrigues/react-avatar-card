cd1f7649b73a233dedf390da789ad7ad0afa7bef
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-25 21:27:57 +0100

    feat: add footer content

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,3 +1,14 @@
 export function Footer() {
-  return <footer>ola</footer>
+  return (
+    <footer className="uppercase text-xs text-sky-300/90 relative">
+      Developed by{' '}
+      <a
+        className="border-b border-sky-300/90 cursor-pointer hover:text-sky-500 hover:border-sky-500 transition-colors"
+        href="https://github.com/gw-rodrigues"
+      >
+        Gleydson W. Rodrigues
+      </a>{' '}
+      | 2023
+    </footer>
+  )
 }
