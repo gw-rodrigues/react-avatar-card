6ddf98ed69a6cf13e54570db6bcbd817e39f1726
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-25 21:28:48 +0100

    feat: add background typep

--- src/contexts/CardContext.tsx
+++ src/contexts/CardContext.tsx
@@ -30,6 +30,7 @@ export interface IMovie {
   original_language: string
   original_title: string
   overview: string
+  backdrop_path: string
   poster_path: string
   release_date: string
   runtime: number
