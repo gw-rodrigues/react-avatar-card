d0cd59ec259830abbf41369f91e8f7e83453299f
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-25 21:28:38 +0100

    feat: remove asset and fix style

--- src/pages/cardList/card/index.tsx
+++ src/pages/cardList/card/index.tsx
@@ -1,7 +1,6 @@
 import { useContextSelector } from 'use-context-selector'
 import { CardContext } from '../../../contexts/CardContext'
 
-import AvatarLogo from '../../../assets/avatar-logo.png'
 import { Rating } from '../../../components/Rating'
 import { Genres } from '../../../components/Genres'
 import { Cast } from '../../../components/Cast'
@@ -9,18 +8,20 @@ import { Cast } from '../../../components/Cast'
 export function Card() {
   const movies = useContextSelector(CardContext, (context) => context.movie)
 
-  if (!movies) return null
+  if (!movies) {
+    return null
+  }
 
   return (
-    <article className="h-[450px] w-80 rounded-2xl shadow-xl overflow-hidden relative [&>div]:hover:before:bottom-[0px] [&>div>img]:hover:blur-sm [&>div>img]:hover:translate-y-[-25px] [&>section]:hover:bottom-0">
-      <div className="relative overflow-hidden content-none before:absolute before:bottom-[-100px] before:w-full before:h-full before:transition-all before:duration-500 before:z-10 before:bg-gradient-to-b before:from-sky-700/0 before:via-sky-700 before:to-sky-700/100 ">
+    <article className="h-[450px] w-80 rounded-2xl shadow-xl drop-shadow-xl overflow-hidden relative  border-2 border-sky-400/20 group">
+      <div className="relative overflow-hidden w-full h-full before:absolute before:bottom-[-100px] before:w-full before:h-full before:transition-all before:duration-500 before:z-10 before:bg-gradient-to-b before:from-sky-700/0 before:via-sky-700 before:to-sky-700/100 group-hover:before:bottom-[0px] ">
         <img
           src={`${movies.urls.posterURL}/${movies.poster_path}`}
           alt="Poster"
-          className="w-full h-auto transition-all duration-500 scale-125"
+          className="w-full h-full transition-all duration-500 relative z-0 group-hover:blur-sm group-hover:translate-y-[-25px]"
         />
       </div>
-      <section className="absolute -bottom-[193px] left-0 z-20 flex flex-col w-full gap-2 p-5 transition-all duration-500 text-white">
+      <section className="absolute -bottom-[215px] left-0 z-30 flex flex-col w-full gap-2 p-5 transition-all duration-500 text-white group-hover:bottom-0">
         <header>
           <a
             href={movies.homepage}
